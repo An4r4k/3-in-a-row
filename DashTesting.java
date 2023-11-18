6ed24c864eb539d410afaa7554133baa5df08dcd
An4r4k <151395267+An4r4k@users.noreply.github.com>
2023-11-18 14:55:01 -0500

    Create DashTesting.java

---
+++ DashTesting.java
@@ -0,0 +1,13 @@
+public class DashTesting{
+	public static void TestDashStuff(){
+		/*
+  		Grid newGrid = new Grid(5,5);
+		for (int i = 0; i < newGrid.rows; i++) {
+			for (int j = 0; j < newGrid.columns; j++) {
+				System.out.print(newGrid.arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		*/
+	}
+}
