701dbaa4a13f0affbe7ee3e3cabb3139e712b907
Molly17018 <151395306+Molly17018@users.noreply.github.com>
2023-11-18 13:01:32 -0500

    start
    start

---
+++ 3InARow/src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Main {
+	
+	   private static void createAndShowGUI() {
+	        JFrame jFrame = new JFrame("3 In a Row");
+	        jFrame.setLayout(new FlowLayout());
+	        jFrame.setSize(500, 360);
+	        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	        JLabel label = new JLabel("Hello World Swing");
+	        Border border = BorderFactory.createLineBorder(Color.BLACK);
+	        label.setBorder(border);
+	        label.setPreferredSize(new Dimension(150, 100));
+
+	        label.setText("3 In a Row");
+
+	        jFrame.add(label);
+	        jFrame.setVisible(true);
+	      
+	    }
+	    
+	  public static void main(String[] args) {
+	    createAndShowGUI();
+	  }
+
+
+}
