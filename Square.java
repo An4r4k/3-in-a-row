91fa0ee0e9e5b2bb38bca36f713f73d8acb60914
An4r4k <151395267+An4r4k@users.noreply.github.com>
2023-11-18 13:28:40 -0500

    Create Square.java
    please work aaaaaaaaaa

---
+++ Square.java
@@ -0,0 +1,13 @@
+public class Square{
+	string currentColor;
+	string correctColor;
+	int gridX;
+	int gridY;
+	public Square(string inputCorrectColor, int inputGridX, int inputGridY){
+		correctColor = inputCorrectColor;
+		currentColor = "grey";
+		gridX = inputGridX;
+		gridY = inputGridY;
+	}
+	//add button functionality
+}
